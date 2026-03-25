3653f3b2a7d80e35767672274bbec764272d7b7b
Jay Marzan <jaymarzan36@gmail.com>
2026-03-24 19:24:54 -0600

    Web version of API doc

---
+++ codepop_backend/orbitdb/doc/site/script.js
@@ -0,0 +1,9 @@
+
+// Smooth scrolling
+document.querySelectorAll('.sidebar a').forEach(link => {
+    link.addEventListener('click', e => {
+        e.preventDefault();
+        document.querySelector(link.getAttribute('href'))
+            .scrollIntoView({ behavior: 'smooth' });
+    });
+});
